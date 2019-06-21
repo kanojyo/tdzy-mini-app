be7f6b668915d414ffe88590d494d26e40737def
zhoujie <673191158@qq.com>
2019-06-21 08:55:07 +0800

    提交代码

--- pages/demo/demo.js
+++ pages/demo/demo.js
@@ -1,4 +1,4 @@
-// pages/healthNews/healthNews.js
+// pages/demo/demo.js
 Page({
 
   /**
@@ -69,11 +69,32 @@ Page({
       },
     ]
   },
+  //获取当前滑块的index
+  bindchange: function (e) {
+    const that = this;
+    that.setData({
+      currentData: e.detail.current
+    })
+  },
+  //点击切换，滑块index赋值
+  checkCurrent: function (e) {
+    const that = this;
+
+    if (that.data.currentData === e.target.dataset.current) {
+      return false;
+    } else {
+
+      that.setData({
+        currentData: e.target.dataset.current
+      })
+    }
+  },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+
   },
 
   /**
@@ -87,55 +108,41 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  },
-  //获取当前滑块的index
-  bindchange: function (e) {
-    const that = this;
-    // console.log(e,111)
-    that.setData({
-      currentData: e.detail.current
-    })
-  },
-  //点击切换，滑块index赋值
-  checkCurrent: function (e) {
-    const that = this;
-    // console.log(e,2222)
-    if (that.data.currentData === e.target.dataset.current) {
-      return false;
-    } else {
-      that.setData({
-        currentData: e.target.dataset.current
-      })
-    }
+
   }
 })
