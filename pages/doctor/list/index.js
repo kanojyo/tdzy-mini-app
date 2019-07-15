97cd2a0e806b6d584dd14e71f5f08e6464fdcf1f
pengjiakai <1845425264@qq.com>
2019-07-15 17:09:18 +0800

    test

--- pages/doctor/list/index.js
+++ pages/doctor/list/index.js
@@ -8,7 +8,10 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    page: 1,
+    navScrollLeft: 0,
+    scrollTop: 0,
+    scrollHeight: 0,
   },
 
   /**
@@ -67,7 +70,50 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
+    var that = this;
+    var page = that.data.page + 1;
+    var status = true;
+    
+    if (status) {
+      wx.showLoading({
+        title: '拼命加载中',
+      });
+      setTimeout(function () {
+        getRequest({
+          url: '/v1/medical_info/recommend_doctor?page_index=' + page,
+          method: 'GET',
+          success(res) {
+            status = false;
+            if (res.data.data.length > 0) {
+              that.data.doctorList = that.data.doctorList.concat(res.data.data);
+              if (res.data.data.length < 10) {
+                that.setData({
+                  bottomTitle: true,
+                  title: '-- 我是有底线的 --',
+                })
+              } else {
+                status = true;
+              }
 
+            } else {
+              wx.showToast({
+                title: '没有更多内容了',
+                icon: 'none',
+                duration: 2000
+              })
+              status = false;
+            }
+
+            that.setData({
+              doctorList: that.data.doctorList,
+              page: page
+            });
+          }
+        });
+        wx.hideLoading();
+      }, 2000);
+    }
+    
   },
 
   /**
@@ -96,26 +142,12 @@ Page({
          }
       }
     })
-
-    // wx.request({
-    //   url: baseUrl + '/v1/appointment/user_info_perfect',
-    //   method: 'GET',
-    //   header: {
-    //     'Content-Type': 'application/json',
-    //     'device': wx.getStorageSync('device'),
-    //     'Authorization': 'Bearer ' + wx.getStorageSync('token')
-    //   },
-    //   success(res) {
-    //     if (!res.data.data.status) {
-    //       wx.navigateTo({
-    //         url: '/pages/doctor/bind/index?id=' + doctor_id
-    //       })
-    //     } else {
-    //       wx.navigateTo({
-    //         url: '/pages/doctor/order/index?id=' + doctor_id,
-    //       })
-    //     }
-    //   }
-    // })
+  },
+  //跳转医生详情
+  doctorInfo: function (e) {
+    var id = e.currentTarget.id;
+    wx.navigateTo({
+      url: "/pages/doctor/info/index?doctor_id=" + id,
+    })
   }
 })
