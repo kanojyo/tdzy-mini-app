520bfd78f6d77a03452328d6a05852c68ebf99f1
zhoujie <673191158@qq.com>
2019-07-31 09:26:30 +0800

    test

--- app.js
+++ app.js
@@ -10,13 +10,14 @@ App({
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-
+    console.log('onlaunch')
     const baseUrl ="https://tdxcx.wuhanlst.com";
     var device ='';
     var token='';
     var session_key='';
     var that = this;
     //获取设备号
+    console.log('device')
     wx.request({
       url: baseUrl +'/v1/device',
       header: {
@@ -25,6 +26,7 @@ App({
       method:'GET',
       dataType:'json',
       success(res){
+        console.log('deviceSuccess')
         device=res.data.data.device;
         //将设备号储存起来
         wx.setStorageSync('device', device)
@@ -36,6 +38,7 @@ App({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
         // console.log(res)
+        console.log('login')
         if (res.code) {
           wx.request({
             url: baseUrl + '/v1/login',
@@ -47,12 +50,19 @@ App({
               'device': wx.getStorageSync('device'),
             },
             success(e){
+              console.log('loginSuccess')
+              that.globalData.token = e.data.data.token;
               token = e.data.data.token;
               session_key = e.data.data.session_key;
               token = e.data.data.token;
               //将token储存起来
               wx.setStorageSync('token', token)
               wx.setStorageSync('session_key', session_key)
+              //由于这里是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况
+              if (that.tokenCallback) {
+                that.tokenCallback(token);
+              }
             }
           })
         }
@@ -61,5 +71,6 @@ App({
   },
   globalData: {
     userInfo: null,
+    token:'',
   }
 })
